90adc9f47bf0dd2786b483e6e7534aa9f7436504
Егор <159357tdutybz99@gmail.com>
2024-07-11 17:11:42 +0300

    update: обработка телеграм requests

--- app/Services/User/TelegramAuthService.php
+++ app/Services/User/TelegramAuthService.php
@@ -11,7 +11,6 @@ use DefStudio\Telegraph\Telegraph;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 
 class TelegramAuthService
 {
@@ -20,8 +19,7 @@ class TelegramAuthService
      * @param Telegraph $telegraph
      * @param TelegramChatService $telegramChatService
      */
-    public function __construct(Telegraph           $telegraph,
-                                TelegramChatService $telegramChatService)
+    public function __construct(Telegraph $telegraph, TelegramChatService $telegramChatService)
     {
         $this->telegramChatService = $telegramChatService;
         $this->telegraph = $telegraph;
@@ -36,66 +34,72 @@ class TelegramAuthService
      */
     public function handleTelegramCallback(int $chatId): bool
     {
-        $bot = TelegraphBot::first();
+        try {
+            $bot = TelegraphBot::first();
 
-        // Получаем данные пользователя через API Telegram
-        $response = Http::post("https://api.telegram.org/bot{$bot->token}/getChat", [
-            'chat_id' => $chatId,
-        ]);
-        $data = $response->json()['result'];
+            // Получаем данные пользователя через API Telegram
+            $response = Http::post("https://api.telegram.org/bot{$bot->token}/getChat", [
+                'chat_id' => $chatId,
+            ]);
+            $data = $response->json()['result'];
 
-        // Валидация данных Telegram
-        if (!$data) {
+            // Валидация данных Telegram
+            if (!$data) {
+                return false;
+            }
+
+            // Проверяем, существует ли пользователь в базе данных
+            $user = User::where('telegram_id', $data['id'])->first();
+            $isNewUser = false;
+            $avatar = null;
+
+            // Если пользователь не существует или у него нет аватара, скачиваем аватар
+            if (!$user || !$user->avatar) {
+                $avatar = isset($data['photo']) ? $this->downloadUserAvatar($bot->token, $data['photo'], $data['id']) : null;
+            }
+
+            // Создаем DTO из данных
+            $telegramUserDTO = new TelegramUserDTO([
+                'name' => $data['first_name'],
+                'telegram_id' => $data['id'],
+                'username' => $data['username'] ?? null,
+                'avatar' => $avatar ?? $user->avatar ?? null, // Используем существующий аватар, если он есть
+            ]);
+
+            // Логика авторизации
+            if (!$user) {
+                $isNewUser = true;
+            }
+
+            // Логика авторизации
+            $user = User::updateOrCreate(
+                ['telegram_id' => $telegramUserDTO->telegram_id],
+                [
+                    'username' => $telegramUserDTO->username,
+                    'name' => $telegramUserDTO->name,
+                    'avatar' => $telegramUserDTO->avatar,
+                ]
+            );
+
+            // Связываем пользователя с чатом
+            $chat = TelegraphChat::where('chat_id', $chatId)->first();
+            if ($chat) {
+                $user->telegraph_chat_id = $chat->id;
+                $user->save();
+            }
+
+            Auth::login($user);
+
+            // Отправляем сообщение пользователю об успешной регистрации, если это новый пользователь
+            if ($isNewUser) {
+                $this->telegramChatService->sendMessage($chatId, 'Регистрация прошла успешно! Добро пожаловать! Теперь вы можете добавить меня к себе на канал! не забудьте выдать мне нужные права');
+            }
+
+            return true;
+        } catch (\Exception $e) {
+            Log::error("Ошибка при обработке Telegram Callback: " . $e->getMessage());
             return false;
         }
-
-        // Проверяем, существует ли пользователь в базе данных
-        $user = User::where('telegram_id', $data['id'])->first();
-        $isNewUser = false;
-        $avatar = null;
-
-        // Если пользователь не существует или у него нет аватара, скачиваем аватар
-        if (!$user || !$user->avatar) {
-            $avatar = isset($data['photo']) ? $this->downloadUserAvatar($bot->token, $data['photo'], $data['id']) : null;
-        }
-
-        // Создаем DTO из данных
-        $telegramUserDTO = new TelegramUserDTO([
-            'name' => $data['first_name'],
-            'telegram_id' => $data['id'],
-            'username' => $data['username'] ?? null,
-            'avatar' => $avatar ?? $user->avatar ?? null, // Используем существующий аватар, если он есть
-        ]);
-        // Логика авторизации
-        if (!$user) {
-            $isNewUser = true;
-        }
-
-        // Логика авторизации
-        $user = User::updateOrCreate(
-            ['telegram_id' => $telegramUserDTO->telegram_id],
-            [
-                'username' => $telegramUserDTO->username,
-                'name' => $telegramUserDTO->name,
-                'avatar' => $telegramUserDTO->avatar,
-            ]
-        );
-
-        // Связываем пользователя с чатом
-        $chat = TelegraphChat::where('chat_id', $chatId)->first();
-        if ($chat) {
-            $user->telegraph_chat_id = $chat->id;
-            $user->save();
-        }
-
-        Auth::login($user);
-
-        // Отправляем сообщение пользователю об успешной регистрации, если это новый пользователь
-        if ($isNewUser) {
-            $this->telegramChatService->sendMessage($chatId, 'Регистрация прошла успешно! Добро пожаловать! Теперь вы можете добавить меня к себе на канал! не забудьте выдать мне нужные права');
-        }
-
-        return true;
     }
 
 
@@ -109,42 +113,47 @@ class TelegramAuthService
      */
     private function downloadUserAvatar(string $botToken, array $photo, int $userId): ?string
     {
-        $fileId = $photo['small_file_id']; // Используем small_file_id для низкого качества
+        try {
+            $fileId = $photo['small_file_id']; // Используем small_file_id для низкого качества
 
-        // Получаем информацию о файле
-        $response = Http::get("https://api.telegram.org/bot{$botToken}/getFile", [
-            'file_id' => $fileId,
-        ]);
+            // Получаем информацию о файле
+            $response = Http::get("https://api.telegram.org/bot{$botToken}/getFile", [
+                'file_id' => $fileId,
+            ]);
 
-        $fileInfo = $response->json();
+            $fileInfo = $response->json();
 
-        if ($fileInfo['ok']) {
-            $filePath = $fileInfo['result']['file_path'];
-            $fileUrl = "https://api.telegram.org/file/bot{$botToken}/{$filePath}";
+            if ($fileInfo['ok']) {
+                $filePath = $fileInfo['result']['file_path'];
+                $fileUrl = "https://api.telegram.org/file/bot{$botToken}/{$filePath}";
 
-            Log::info("URL файла для скачивания: {$fileUrl}");
+                Log::info("URL файла для скачивания: {$fileUrl}");
 
-            // Скачиваем файл
-            $fileContent = Http::get($fileUrl)->body();
+                // Скачиваем файл
+                $fileContent = Http::get($fileUrl)->body();
 
-            // Генерируем имя файла и сохраняем его в директорию public
-            $fileName = "images/userAvatars/{$userId}.jpg";
-            $publicPath = public_path($fileName);
+                // Генерируем имя файла и сохраняем его в директорию public
+                $fileName = "images/userAvatars/{$userId}.jpg";
+                $publicPath = public_path($fileName);
 
-            // Проверяем директорию
-            $directory = public_path('images/userAvatars');
-            if (!file_exists($directory)) {
-                mkdir($directory, 0777, true);
+                // Проверяем директорию
+                $directory = public_path('images/userAvatars');
+                if (!file_exists($directory)) {
+                    mkdir($directory, 0777, true);
+                }
+
+                // Сохраняем файл
+                file_put_contents($publicPath, $fileContent);
+
+                Log::info("Файл успешно сохранен: {$publicPath}");
+
+                return "/images/userAvatars/{$userId}.jpg";
+            } else {
+                Log::error("Ошибка при получении информации о файле: " . $fileInfo['description']);
+                return null;
             }
-
-            // Сохраняем файл
-            file_put_contents($publicPath, $fileContent);
-
-            Log::info("Файл успешно сохранен: {$publicPath}");
-
-            return "/images/userAvatars/{$userId}.jpg";
-        } else {
-            Log::error("Ошибка при получении информации о файле: " . $fileInfo['description']);
+        } catch (\Exception $e) {
+            Log::error("Ошибка при скачивании аватара пользователя: " . $e->getMessage());
             return null;
         }
     }
--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -7,76 +7,168 @@ use App\Models\StaticCommand;
 use App\Services\Channel\TelegramChannelService;
 use App\Services\Chat\TelegramChatService;
 use App\Services\User\TelegramUserService;
-use DefStudio\Telegraph\DTO\Chat;
+use DefStudio\Telegraph\DTO\CallbackQuery;
+use DefStudio\Telegraph\DTO\Chat as DTOChat;
+use DefStudio\Telegraph\DTO\InlineQuery;
+use DefStudio\Telegraph\DTO\Message;
 use DefStudio\Telegraph\DTO\User;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 use DefStudio\Telegraph\Models\TelegraphBot;
+use DefStudio\Telegraph\Models\TelegraphChat;
 use DefStudio\Telegraph\Telegraph;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use App\Models\Channel;
-use Illuminate\Support\Facades\Auth;
-use DefStudio\Telegraph\DTO\Message;
 use Illuminate\Support\Stringable;
+use Throwable;
 
-
-#[AllowDynamicProperties] class Handler extends WebhookHandler
+#[AllowDynamicProperties]
+class Handler extends WebhookHandler
 {
+    protected TelegramChannelService $telegramChannelService;
+    protected TelegramUserService $telegramUserService;
+    protected TelegramChatService $telegramChatService;
+    protected Telegraph $telegraph;
+    protected TelegraphBot $bot;
+    protected Request $request;
+    protected ?Message $message = null;
+    protected ?CallbackQuery $callbackQuery = null;
+    protected TelegraphChat $chat;
+
     public function __construct(
         TelegramChannelService $telegramChannelService,
         TelegramUserService    $telegramUserService,
         TelegramChatService    $telegramChatService,
-        Telegraph              $telegraph)
-
+        Telegraph              $telegraph
+    )
     {
         $this->telegramChannelService = $telegramChannelService;
         $this->telegramUserService = $telegramUserService;
         $this->telegramChatService = $telegramChatService;
         $this->telegraph = $telegraph;
-
     }
 
-    public function hello()
-    {
-        $this->reply('Привет! Я самый крутой бот!');
-    }
-
-    protected function handleUnknownCommand(Stringable $text): void
+    /**
+     * Неизвестная команда
+     *
+     * @param string|Stringable $text
+     * @return void
+     */
+    protected function handleUnknownCommand(string|Stringable $text): void
     {
         $this->reply('Неизвестная команда');
     }
 
+    /**
+     * Команда /start
+     *
+     * @return void
+     */
     public function start()
     {
         $chatId = $this->chat->chat_id;
-
         $registrationUrl = route('telegram.auth.callback', ['chat_id' => $chatId]);
         $this->reply("Добро пожаловать! Перед использованием нашего бота нужно авторизоваться, для этого пройдите по кнопке ниже:\n[Авторизация]($registrationUrl)");
     }
-
 
     /**
      * @param Request $request
      * @param TelegraphBot $bot
      * @return void
+     * @throws Throwable
      */
     public function handle(Request $request, TelegraphBot $bot): void
     {
-        $data = $request->all();
-        if (isset($data['message']['text'])){
-            $command = StaticCommand::where('command', $data['message']['text'])->first();
+        try {
+            $this->bot = $bot;
+            $this->request = $request;
+
+            $data = $request->all();
+
+            if ($this->request->has('message')) {
+                $this->message = Message::fromArray($this->request->input('message'));
+                $this->chat = $this->convertToTelegraphChat($this->message->chat());
+                $this->handleMessage();
+                return;
+            }
+
+            if ($this->request->has('edited_message')) {
+                $this->message = Message::fromArray($this->request->input('edited_message'));
+                $this->chat = $this->convertToTelegraphChat($this->message->chat());
+                $this->handleMessage();
+                return;
+            }
+
+            if ($this->request->has('channel_post')) {
+                $this->message = Message::fromArray($this->request->input('channel_post'));
+                $this->chat = $this->convertToTelegraphChat($this->message->chat());
+                $this->handleMessage();
+                return;
+            }
+
+            if ($this->request->has('callback_query')) {
+                $this->callbackQuery = CallbackQuery::fromArray($this->request->input('callback_query'));
+                $this->chat = $this->callbackQuery->message()?->chat() ? $this->convertToTelegraphChat($this->callbackQuery->message()->chat()) : null;
+                $this->handleCallbackQuery();
+            }
+
+            if ($this->request->has('inline_query')) {
+                $this->handleInlineQuery(InlineQuery::fromArray($this->request->input('inline_query')));
+            }
+
+            if (isset($data['my_chat_member']) && $data['my_chat_member']['chat']['type'] === 'channel') {
+                $this->handleChannelMember($data, $bot);
+            } elseif (isset($command)) {
+                Log::info('Executing static command: ' . $command->command);
+                $staticCommand = StaticCommand::where('command', $data['message']['text'])->first();
+                $this->telegramChatService->sendMessage($data['message']['chat']['id'], $staticCommand->message);
+            } else {
+                $this->handleUnknownCommand($data['message']['text']);
+            }
+        } catch (Throwable $throwable) {
+            $this->onFailure($throwable);
         }
-        if (isset($data['my_chat_member']) && $data['my_chat_member']['chat']['type'] === 'channel') {
-            $this->handleChannelMember($data, $bot);
+    }
 
-        } elseif (isset($command)) {
-            $staticCommand = StaticCommand::where('command', $data['message']['text'])->first();
-
-            // Отправляем сообщение, если команда найдена
-            $this->telegramChatService->sendMessage($data['message']['chat']['id'], $staticCommand->message);
+    /**
+     * обработка всех команд
+     *
+     * @return void
+     */
+    protected function handleMessage(): void
+    {
+        if ($this->message->text() === '/start') {
+            $this->start();
         } else {
-            parent::handle($request, $bot);
+            $command = StaticCommand::where('command', $this->message->text())->first();
+            if ($command) {
+                $this->telegramChatService->sendMessage($this->chat->chat_id, $command->message);
+            } else {
+                $this->handleUnknownCommand($this->message->text());
+            }
         }
+    }
+
+    /**
+     * Взаимодейтсвие с кнопками
+     *
+     * @return void
+     */
+    protected function handleCallbackQuery(): void
+    {
+        // Логика обработки callback-запроса
+        $this->reply('Callback query received');
+    }
+
+    /**
+     * Взаимодействие с текстом
+     *
+     * @param InlineQuery $inlineQuery
+     * @return void
+     */
+    protected function handleInlineQuery(InlineQuery $inlineQuery): void
+    {
+        // Логика обработки inline-запроса
+        $this->reply('Inline query received');
     }
 
     /**
@@ -84,29 +176,38 @@ use Illuminate\Support\Stringable;
      * @param TelegraphBot $bot
      * @return void
      */
-    protected
-    function handleChannelMember(array $data, TelegraphBot $bot): void
+    protected function handleChannelMember(array $data, TelegraphBot $bot): void
     {
         $chatUserId = $data['my_chat_member']['from']['id'];
 
-        //При добавление бота на канал
         if (isset($data['my_chat_member']['new_chat_member']['status']) && $data['my_chat_member']['new_chat_member']['status'] === 'administrator') {
             $message = $this->telegramChannelService->addChannel($data, $bot);
-
             $this->telegramChatService->sendMessage($chatUserId, $message);
-
         }
-        //При удаление бота с канала
 
         if (isset($data['my_chat_member']['new_chat_member']['status']) && $data['my_chat_member']['new_chat_member']['status'] === 'left') {
             Log::info('Обнаружен статус выхода.');
             $channelName = $data['my_chat_member']['chat']['title'];
             $chatUserId = $this->telegramChatService->getChatIdByChannelId($data['my_chat_member']['chat']['id']);
             if ($chatUserId) {
-                $this->telegramChatService->sendMessage($chatUserId, 'Бот был удален с канала!');
+                $this->telegramChatService->sendMessage($chatUserId, "Бот был удален с канала! $channelName");
             } else {
                 Log::warning("Не удалось найти chat_id");
             }
         }
+    }
+
+    /**
+     * @param DTOChat $dtoChat
+     * @return TelegraphChat
+     */
+    private function convertToTelegraphChat(DTOChat $dtoChat): TelegraphChat
+    {
+        // Создаем новый экземпляр TelegraphChat и заполняем его данными из DTOChat
+        $telegraphChat = new TelegraphChat();
+        $telegraphChat->chat_id = $dtoChat->id();
+        $telegraphChat->name = $dtoChat->title();
+
+        return $telegraphChat;
     }
 }
